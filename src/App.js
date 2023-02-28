9b2f0be3b926f1ffd547a5d2bef594ae215314cf
MinhQuang <minhquanga32001@gmail.com>
2023-02-28 20:31:13 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DefaultLayout from "./Layouts/DefaultLayout/DefaultLayout";
+import "./Assets/Styles/Styles.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <DefaultLayout />;
+};
 
 export default App;
